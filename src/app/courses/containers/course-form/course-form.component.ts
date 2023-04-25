238d810e9dcf6364cd383b68decdcdbf11cdbbea
brunoclaumari <brunoclaumari@gmail.com>
2023-04-24 23:56:36 -0300

    Validações no frontend

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { catchError, of } from 'rxjs';
@@ -11,89 +11,96 @@ import { Course } from './../../model/course';
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent implements OnInit {
-
   //form: UntypedFormGroup;
   form = this.formBuilder.group({
-    _id:[''],
-    name: [''],
-    category: ['']
+    _id: [''],
+    name: [
+      '',
+      [Validators.required, Validators.minLength(5), Validators.maxLength(100)],
+    ],
+    category: ['', [Validators.required]],
   });
 
-  selected: String
+  selected: String;
 
-  constructor(private formBuilder:NonNullableFormBuilder,
-    private coursesService:CoursesService,
+  constructor(
+    private formBuilder: NonNullableFormBuilder,
+    private coursesService: CoursesService,
     private snackBar: MatSnackBar,
-    private router:Router,
-    private activeRoute:ActivatedRoute,
-    private location:Location
-    ) {
-
+    private router: Router,
+    private activeRoute: ActivatedRoute,
+    private location: Location
+  ) {
     this.selected = 'option1';
   }
 
   ngOnInit(): void {
-    const course:Course = this.activeRoute.snapshot.data['course'];
+    const course: Course = this.activeRoute.snapshot.data['course'];
     console.log(course);
     this.updateForm(course);
-/*     this.activeRoute.params.subscribe(
-      (params: any)=>{
-        if(Number.parseInt(params['_id']) > 0){
-          console.log(`id: ${params['_id']}`);
-          this._id = params['_id'];
-          console.log(this._id);
-          const curso$ = this.coursesService.loadById(this._id);
-          curso$.subscribe(curso =>{
-            this.updateForm(curso);
-            console.log(curso);
-          });
-        }
-      }
-    ); */
   }
 
-  updateForm(curso:Course){
+  updateForm(curso: Course) {
     this.form.patchValue({
       _id: curso._id,
       name: curso.name,
-      category: curso.category
+      category: curso.category,
     });
   }
 
-  onSubmit(){
+  onSubmit() {
     //alert('aoooba')
     console.log(`Form: ${JSON.stringify(this.form.value)}`);
-    this.coursesService.save(this.form.value)
-    .pipe(
-      catchError(erro =>{
-        this.openSnackBar('Algo deu errado!!', '')
-        return of([]);
-      })
-    )
-    .subscribe(result => {
-      console.log(`Result: ${JSON.stringify(result)}`);
-      this.router.navigate(['']);//{relativeTo:this.activeRoute}
-      this.onSuccess(this.form.value);
-    });
+    this.coursesService
+      .save(this.form.value)
+      .pipe(
+        catchError((erro) => {
+          this.openSnackBar('Algo deu errado!!', '');
+          return of([]);
+        })
+      )
+      .subscribe((result) => {
+        console.log(`Result: ${JSON.stringify(result)}`);
+        this.router.navigate(['']); //{relativeTo:this.activeRoute}
+        this.onSuccess(this.form.value);
+      });
   }
 
-  onSuccess(result:Partial<Course>){
+  onSuccess(result: Partial<Course>) {
     //msgSuccess = 'Curso salvo com sucesso';
-    this.openSnackBar(`Curso "${result.name}" salvo com sucesso! `,'');
+    this.openSnackBar(`Curso "${result.name}" salvo com sucesso! `, '');
   }
 
   openSnackBar(message: string, action: string) {
-    this.snackBar.open(message, action,{
+    this.snackBar.open(message, action, {
       duration: 5000,
       //panelClass: ['green-snackbar','green'],
     });
   }
 
-  onCancel(){
+  onCancel() {
     this.location.back();
   }
 
+  getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
+    if (field?.hasError('required')) {
+      return `Campo "${fieldName}" é obrigatório!`;
+    }
+
+    if (field?.hasError('minlength')) {
+      const requiredLength:number = field.errors? field.errors['minlength']['requiredLength']:5;
+      return `Tamanho mínimo tem que ser ${requiredLength} caracteres!`;
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredLength:number = field.errors? field.errors['maxlength']['requiredLength']:100;
+      return `Tamanho máximo tem que ser ${requiredLength} caracteres!`;
+    }
+
+    return 'Campo inválido';
+  }
 }
